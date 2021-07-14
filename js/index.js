cc7e14772be94be6e48968f6350036cd34e443c2
Andy Liu <aliu927@gmail.com>
2021-07-14 00:12:24 -0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,52 +1,58 @@
 const modalContainer = document.querySelector(".modalContainer");
-const btn = document.querySelectorAll(".select_btn");
+const buttons = document.querySelectorAll(".select_btn");
 
-for(let i = 0; i < btn.length; i++){
-  btn[i].addEventListener("click", e =>{
+/* for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", (e) => {
     // console.log(e);
     modalContainer.style.display = "block";
+    const checkedRadio = document.querySelectorAll(".radio");
+    for (let j = 0; j < checkedRadio.length; j++) {
+      checkedRadio[j].addEventListener("click", (e) => {
+        //console.log(checkedRadio[j].checked);
+        const moneyContainer = document.querySelectorAll(".money_container");
+        if (checkedRadio[j].checked) {
+          console.log(moneyContainer[j]);
+          moneyContainer[j].style.display = "flex";
+        }
+      });
+    }
   });
-};
+}
+ */
 
-const close_Btn = document.querySelector('.close_container');
-close_Btn.addEventListener('click',e=>{
-  modalContainer.style.display = 'none';
+for (const btnElement of buttons) {
+  btnElement.addEventListener("click", (e) => {
+    modalContainer.style.display = "block";
+    const radios = document.querySelectorAll(".radio");
+    for (const radio of radios) {
+      radio.addEventListener("change", (e) => {
+        console.log(e);
+      });
+    }
+  });
+}
+
+const close_Btn = document.querySelector(".close_container");
+close_Btn.addEventListener("click", (e) => {
+  modalContainer.style.display = "none";
 });
 
-const progressbar = document.querySelector('.pBar');
+const progressbar = document.querySelector(".pBar");
 const currentValue = 89914;
 const maxValue = 100000;
 
-progressbar.setAttribute('value',currentValue);
-progressbar.setAttribute('max',maxValue);
-
-// When money gets added thru modal, currentValue + addedValue = new CurrentValue;
-
-const modalBtn = document.querySelectorAll(".btn_submit");
-const inputValue = document.querySelectorAll('.input_amt');
-for(let i = 0; i < modalBtn.length;i++){
- modalBtn[i].addEventListener('click',e =>{
-    for(let j = 0; j < inputValue.length;j++){
-      console.log(inputValue[j].value)
-      //write a condition that checks each input value. 
-    };
- });
-};
-// When Continue is clicked, get value
-// After value is retrieved add to currentValue. 
-// For Bamboo stand and Black stand, innerText Value reduced by 1 for every click.
-
+progressbar.setAttribute("value", currentValue);
+progressbar.setAttribute("max", maxValue);
 
 /* 
+livid46: you need to add an event listener for 'change'
+livid46: that way, it fires for both check or unchecked
+livid46: instead of click
+livid46: and simply check the event.target or event.srcElement
+*/
 
-for(modalBtn){
-  let someValue;
-  for(inputValue){
-    someValue = input[i].value
-  }
-}
-
-
-https://www.w3schools.com/jsref/prop_checkbox_checked.asp
-
+/* 
+RyMeRcYbLe: i would create variable store some unique identifier 
+and everytime check if newly clicked one is same or different 
+and if its different then query previous one and hide it
 */
