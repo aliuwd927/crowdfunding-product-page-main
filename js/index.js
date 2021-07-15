476ca07c31222a621bca35c72e9af3d0f6c9e17d
Andy Liu <aliu927@gmail.com>
2021-07-15 00:45:19 -0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,33 +1,22 @@
+//Radio Buttons and Modal
 const modalContainer = document.querySelector(".modalContainer");
 const buttons = document.querySelectorAll(".select_btn");
-
-/* for (let i = 0; i < btn.length; i++) {
-  btn[i].addEventListener("click", (e) => {
-    // console.log(e);
-    modalContainer.style.display = "block";
-    const checkedRadio = document.querySelectorAll(".radio");
-    for (let j = 0; j < checkedRadio.length; j++) {
-      checkedRadio[j].addEventListener("click", (e) => {
-        //console.log(checkedRadio[j].checked);
-        const moneyContainer = document.querySelectorAll(".money_container");
-        if (checkedRadio[j].checked) {
-          console.log(moneyContainer[j]);
-          moneyContainer[j].style.display = "flex";
-        }
-      });
-    }
-  });
-}
- */
 
 for (const btnElement of buttons) {
   btnElement.addEventListener("click", (e) => {
     modalContainer.style.display = "block";
-    const radios = document.querySelectorAll(".radio");
-    for (const radio of radios) {
-      radio.addEventListener("change", (e) => {
-        console.log(e);
-      });
+  });
+}
+
+const radios = document.querySelectorAll(".radio");
+for (const radio of radios) {
+  radio.addEventListener("change", (e) => {
+    const modalContent = document.querySelectorAll(".modal_content");
+    for (const content of modalContent) {
+      let checkedRadio = content.querySelector(".radio:checked");
+      content.querySelector(".money_container").style.display = checkedRadio
+        ? "flex"
+        : "none";
     }
   });
 }
@@ -37,6 +26,21 @@ close_Btn.addEventListener("click", (e) => {
   modalContainer.style.display = "none";
 });
 
+//Progress Bar
+
+//btn_submit onclick, retrieves value from input_amt
+const modalSubmitButtons = modalContainer.querySelectorAll(".btn_submit");
+for (const button of modalSubmitButtons) {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    const inputValues = modalContainer
+      .querySelectorAll("section")
+      .forEach((section) => {
+        console.log(section.querySelector("button"));
+      });
+  });
+}
+
 const progressbar = document.querySelector(".pBar");
 const currentValue = 89914;
 const maxValue = 100000;
@@ -45,14 +49,25 @@ progressbar.setAttribute("value", currentValue);
 progressbar.setAttribute("max", maxValue);
 
 /* 
-livid46: you need to add an event listener for 'change'
-livid46: that way, it fires for both check or unchecked
-livid46: instead of click
-livid46: and simply check the event.target or event.srcElement
+
+- See an updated progress bar and total money raised based on their pledge total after confirming a pledge
+- See the number of total backers increment by one after confirming a pledge
+- Toggle whether or not the product is bookmarked
+
 */
 
 /* 
-RyMeRcYbLe: i would create variable store some unique identifier 
-and everytime check if newly clicked one is same or different 
-and if its different then query previous one and hide it
+
+P_G_: like querySelectorAll('section').forEach(section) { section.querySelector('button'); })
+
+modalContainer.querySelectorAll('.class-of-a-section').forEach(section => {
+  // here 'section' is now the thing you work on
+  const button = section.querySelector('.my-button');
+  const input = section.querySelector('.my-input');
+ 
+  // TODO: do something with it
+});
+
+
+
 */
